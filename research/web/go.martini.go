5d9d83c30f588f72dbee46507849aca774b7bc6b
winlin <winlin@vip.126.com>
2014-11-03 14:22:21 +0800

    update go martini

--- research/web/go.martini.go
+++ research/web/go.martini.go
@@ -1,11 +1,23 @@
 package main
 import (
+    "os"
+    "fmt"
     "encoding/json"
     "net/http"
     "github.com/go-martini/martini"
 )
 
 func main() {
+    fmt.Println("go martini web server")
+    if len(os.Args) <= 1 {
+        fmt.Println("Usage:", os.Args[0], "<port>")
+        fmt.Println("   port: the port to listen at.")
+        fmt.Println("For example:")
+        fmt.Println("   ", os.Args[0], 8080)
+        return
+    }
+    fmt.Println("listen at", os.Args[1])
+    
     m := martini.Classic()
     m.Get("/api/v3/json", func(res http.ResponseWriter) string {
         res.Header().Add("Server", "GoMartini/1.0")
@@ -17,5 +29,5 @@ func main() {
         }
         return string(b)
     })
-    http.ListenAndServe(":8080", m)
+    http.ListenAndServe(":" + os.Args[1], m)
 }
